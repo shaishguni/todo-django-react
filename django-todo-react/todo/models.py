8a8d2f82387963aad0381964f2299dfca3e77fa1
Shaish Guni <gunisguni2007@gmail.com>
2022-01-21 09:25:44 +0545

    admin registration is done

--- django-todo-react/todo/models.py
+++ django-todo-react/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Todo(models.model):
+class Todo(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
     done = models.BooleanField(default=False)
